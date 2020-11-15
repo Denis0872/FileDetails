25213619959da3058f9d7db8812e8bb215d5b25c
denis0872 <denispetergof@gmail.com>
2020-11-15 23:04:11 +0300

    start

--- StringArgs-Task2.1/Program.cs
+++ StringArgs-Task2.1/Program.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StringArgs_Task2._1
 {
-    class Program
+    class FileDetails
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(args.Length);
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            string fileName = args[0];
+            FileStream stream = new FileStream(fileName, FileMode.Open);
+            StreamReader reader = new StreamReader(stream);
+            int size = (int)stream.Length;
+            char[] contents = new char[size];
+            for (int i = 0; i < size; i++)
+            {
+                contents[i] = (char)reader.Read();
+            }
+            foreach (char ch in contents)
+            {
+                Console.Write(ch);
 
-
-
+                Console.ReadKey();
         }
+        
     }
 }
